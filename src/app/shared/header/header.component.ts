2e5c89451c336dd79fe5ca10358ab4c082cca2bb
AlmaDividida <david_626180497@hotmail.com>
2022-03-22 00:42:31 -0600

    Maquetar diseño responsivo de la LandingPage

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,26 +6,10 @@ import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/co
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  @ViewChild('header') header: ElementRef | undefined;
 
-
-  constructor(private renderer: Renderer2) { }
+  constructor() { }
 
   ngOnInit(): void {
-    
-  }
-
-  headerChanger(){
-    let rend = this.renderer;
-    let element = this.header?.nativeElement;
-    window.onscroll = function() {
-      "use strict";
-      if (document.body.scrollTop >= 280 || document.documentElement.scrollTop >= 280) {
-        rend.addClass(element, "scroll-header")
-      } else {
-        rend.removeClass(element, "scroll-header")
-      }
-    };
   }
 
 }
