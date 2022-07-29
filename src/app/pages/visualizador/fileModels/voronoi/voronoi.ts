a73e5f2b899e2d71d60395f7683bfec62aba0010
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2022-07-29 11:03:04 -0500

    Quita puntos con coordenadas en z

--- src/app/pages/visualizador/fileModels/voronoi/voronoi.ts
+++ src/app/pages/visualizador/fileModels/voronoi/voronoi.ts
@@ -22,7 +22,10 @@ export class Voronoi implements FileModelInterface{
     id = this.dateId.getTime();
 
     // El gran, tremendo, inmaculado, semental y poderosísimo constructor
-    constructor(public json:any, public canvas:any){}
+    constructor(public json:any, public canvas:any){
+        this.json = json;
+        this.canvas = canvas;
+    }
 
     // Lista de elementos
     puntosRed: any[] = [];
@@ -63,9 +66,13 @@ export class Voronoi implements FileModelInterface{
         // Recorremos todos los puntos
         puntos.forEach(function (punto:any) {
             // Convertimos las coordenadas a enteros
+            var pz = parseInt(punto.z);
+
+            // Si el punto tiene coordenada z, no dibujarla
+            if (pz != 0) { return };
+
             var px = parseInt(punto.x);
             var py = parseInt(punto.y);
-            var pz = parseInt(punto.z);
 
             // Los puntos se agrupan con esta propiedad
             var sb = '' + punto.sb
@@ -155,6 +162,129 @@ export class Voronoi implements FileModelInterface{
         animate();
     } // Fin draw(json: any, c: any)
 
+    // Dibujar los puntos dados en el JSON
+    draw2d(json: any, c: any): void {
+        console.log(json);
+
+        while(this.scene.children.length > 0){ 
+            this.scene.remove(this.scene.children[0]); 
+        }
+
+        var mySelf = this;
+        // Obtenemos los puntos del json
+        // p: {x: 0, y: 0, z: 0, sb: 502}
+        var puntos = json.p;
+
+        // Posición inicial de la cámara
+        this.camera.position.set(350,350,350);
+
+        // Aplicamos la propidad para auto rotar
+        this.controls.autoRotate = this.isAutoRotating;
+
+        // Lista de las posiciones de los puntos
+        var posiciones: any = {};
+        var positions: any = {};
+        // Lista de los grupos dados por el campo p.sb
+        var cs: any[] = [];
+        // Máximas coordenadas, se actualizan dependiendo los datos
+        var mx = -10000, my = -10000, mz = -10000;
+        
+        // Recorremos todos los puntos
+        puntos.forEach(function (punto:any) {
+            // Convertimos las coordenadas a enteros
+            var px = parseInt(punto.x);
+            var py = parseInt(punto.y);
+            // var pz = parseInt(punto.z);
+            var pz = 0;
+
+            // Los puntos se agrupan con esta propiedad
+            var sb = '' + punto.sb
+
+            // Guardamos las sb para saber los grupos que tenemos
+            if (!cs.includes(sb)){
+                cs.push(sb);
+            }
+
+            // Agregamos la posición al diccionario de coordenadas con su grupo
+            if (!posiciones[sb]){
+                posiciones[sb] = new Array(px, py, pz);
+            }else{
+                posiciones[sb].push(px, py, pz);
+            }
+
+            // Actualizamos las coordenadas máximas
+            if (px > mx) mx = px;
+            if (py > my) my = py;
+            // if (pz > mz) mz = pz;
+
+        }); // FIN puntos.forEach()
+
+        // Esto debe cambiar debido al cambio a BufferGeometry
+        cs.forEach((csColor) => {
+            // Geometría que va contener todos los puntos con el mismo grupo
+            var geometry = new THREE.BufferGeometry();
+
+            // NOTE: LUIS: Por alguna razón el render seleccionado no acepta enteros
+            // así que los convierto en flotantes
+            var positions = Float32Array.from(posiciones[csColor]);
+
+            // Le mandamos al BufferGeometry las coordenadas de los puntos
+            // El objeto creado automáticamente convierte un Array en puntos de 3
+            // coordenadas
+            geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+
+            // Creamos el color del area
+            var pointColor = new THREE.Color( csColor * 111111);
+
+            // Creamos un material para los puntos
+            // TODO: El tamaño de los puntos lo puse a proposito para que se vea
+            // bien, tal vez sea necesario que sea especificado desde el JSON
+            var material = new THREE.PointsMaterial( { size: 2, color: pointColor } );
+
+            // NOTE: LUIS: Si algún día surge un problema puede ser por esta función
+            // geometry.computeBoundingSphere();
+            
+            // Creamos todos los puntos y los agregamos a la escena
+            var points = new THREE.Points( geometry, material );
+            mySelf.scene.add(points);
+
+            // TODO:
+            mySelf.puntosRed.push(points);
+        }); // FIN cs.forEach()
+
+        function resizeCanvasToDisplaySize() {
+            const canvas = mySelf.renderer.domElement;
+            const width = canvas.clientWidth;
+            const height = canvas.clientHeight;
+            
+            if (canvas.width !== width || canvas.height !== height) {
+                mySelf.renderer.setSize(width, height, false);
+                mySelf.camera.aspect = width / height;
+                mySelf.camera.updateProjectionMatrix();
+            }
+        }
+
+        function animate() {
+            requestAnimationFrame( animate );
+            resizeCanvasToDisplaySize()
+            mySelf.controls.update();
+            mySelf.renderer.render(mySelf.scene, mySelf.camera);
+        }
+
+        // Calcular el centro del cubo para la rotación
+        var centro = new THREE.Vector3();
+        centro.x = mx / 2;
+        centro.y = my / 2;
+        centro.z = mz / 2;
+        mySelf.controls.target = centro;
+
+        // Zoom máximo y mínimo
+        mySelf.controls.minDistance = centro.length();
+        mySelf.controls.maxDistance = 1000;
+
+        animate();
+    } // Fin draw2d(json: any, c: any)
+
     // Función para crear el menú y sus respectivos eventos
     mostrarMenu(idVisualizador:any):void {
         /*var vdate:any = new Date();
@@ -202,6 +332,13 @@ export class Voronoi implements FileModelInterface{
                  "<span> Auto Rotate </span>" +
                "</div>"+
              "</li>"+
+             "<li class='nav-item'>"+
+               "<div class='form-check'>"+
+                 "<input type='checkbox' class='form-check-input' id='dosDimension'>"+
+                 "<label class='form-check-label' for='dosDimension'><span></span></label>" +
+                 "<span> 2D </span>" +
+               "</div>"+
+             "</li>"+
            "</ul>"+
          "</div>"
 
@@ -236,12 +373,21 @@ export class Voronoi implements FileModelInterface{
             // Al seleccionar el checkBox llamado Rotar el diagrama comenzará a girar
             $('#autoRotar').change(function(){
                 var check:any = document.getElementById('autoRotar');
+                // BUG: Posible bug
+                mySelf.autoRotar(check);
+                // if(check.checked) {
+                // }else{
+                //     mySelf.autoRotar(check);
+                // }
+            }),
+            $('#dosDimension').change(function(){
+                var check:any = document.getElementById('autoRotar');
                 if(check.checked) {
-                    mySelf.autoRotar(check);
+                    mySelf.draw2d(mySelf.json, mySelf.canvas);
                 }else{
-                    mySelf.autoRotar(check);
+                    // mySelf.autoRotar(check);
                 }
-            })    
+            })
         );
     }// FIN mostrarMenu()
 
