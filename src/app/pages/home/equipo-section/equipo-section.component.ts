307428bbf1b8609ffd27e1b82bff6cd0d205016c
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2022-07-31 15:54:37 -0500

    Agrega botón +Info de investigadores

--- src/app/pages/home/equipo-section/equipo-section.component.ts
+++ src/app/pages/home/equipo-section/equipo-section.component.ts
@@ -12,17 +12,40 @@ export class EquipoSectionComponent implements OnInit {
   Researchers: any;
   Developers: any;
 
-  showInfoStatus: boolean = false;
+  showInfoStatus: boolean[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
     this.Researchers = this.getDatos()[0];
     this.Developers = this.getDatos()[1];
+
+
+    for (let i = 0; i < this.Researchers.length; i++){
+      this.showInfoStatus.push(false);
+    }
   }
 
   getDatos(){
     return [this.Team.Researchers, this.Team.Developers];
   }
 
+  toggleShowInfo(i: number){
+    this.showInfoStatus[i] = !this.showInfoStatus[i];
+    console.log(this.showInfoStatus);
+  }
+
+  getInfoStatus(i: number){
+    return this.showInfoStatus[i];
+  }
+
+  getClassInfo(i: number){
+    var elem = this.showInfoStatus[i];
+
+    if (elem){
+      return "accordion-info-container active";
+    } else {
+      return "accordion-info-container";
+    }
+  }
 }
